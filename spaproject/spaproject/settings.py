ab1969ed36bc7276b91fb21b779ad350160411ab
ImAcon1te <144747662+ImAcon1te@users.noreply.github.com>
2024-01-11 03:40:49 +0200

    Add files via upload

--- spaproject/spaproject/settings.py
+++ spaproject/spaproject/settings.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = [
     'comments',
     'captcha',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    )}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- spaproject/spaproject/urls.py
+++ spaproject/spaproject/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from comments.views import CommentImageView
+from comments.views import CommentImageView, AvatarImageView
 
 urlpatterns = [
     path('api/', include('comments.urls')),
     path('admin/', admin.site.urls),
     path('comment_images/<str:filename>', CommentImageView.as_view(), name='download-file'),
+    path('profile/<int:pk>', AvatarImageView.as_view(), name='avatar-download'),
     # ...
 ]
 
